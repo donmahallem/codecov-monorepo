f551a82afd467222884fb1c755fc673694bfff7e
DonMahallem <donmahallem@users.noreply.github.com>
2021-03-05 02:49:57 +0100

    chore: use node12

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 /*!
  * Source https://github.com/donmahallem/codecov-monorepo
  */
-import { readFileSync } from 'fs';
+import { exec } from '@actions/exec';
+import axios from 'axios';
+import { promises as fsp } from 'fs';
 import { sync } from 'glob';
+
+const downloadBash = async (): Promise<void> => {
+    const data: string = await (await axios('https://codecov.io/bash', { method: 'GET' })).data;
+    await fsp.writeFile('./codecov', data);
+}
+
 export const run = async (): Promise<void> => {
+    await downloadBash();
     const lernaPackage: {
         packages: string[],
-    } = JSON.parse(readFileSync('./lerna.json', 'utf8'));
+    } = JSON.parse(await fsp.readFile('./lerna.json', 'utf8'));
+
     const projectPaths: string[] = [];
     for (const packagePath of lernaPackage.packages) {
         projectPaths.push(...sync(packagePath));
     }
     console.log('Paths found', projectPaths);
+    await exec('./codecov', ['-f', 'kus']);
 };
