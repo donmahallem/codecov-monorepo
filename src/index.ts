ef4c5c57d1ddf5c050828acc1b959cede82eedec
DonMahallem <donmahallem@users.noreply.github.com>
2021-03-05 02:00:50 +0100

    chore: bump

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 /*!
  * Source https://github.com/donmahallem/codecov-monorepo
  */
+import { error, info } from '@actions/core';
+import { run } from './app';
 
-export class Yet {
-    public nope(): void {
-
-    }
-}
+run()
+    .then((): void => {
+        info('Success');
+    })
+    .catch((err: any): void => {
+        error(err?.message || 'Error occured');
+    });
